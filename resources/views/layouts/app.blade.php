e34c2ab8558dea7d13b89137d29b43dc8994d315
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-08 17:42:21 +1000

    HTML profile template.
    This is very basic. The routes obviously don't exist yet.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,11 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
+                                        <a href="{{ route('profile') }}">
+                                            Profile
+                                        </a>
+                                    </li>
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,10 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Profile</div>
+                <div class="panel-heading">{{ Auth::user()->name }}'s Profile</div>
 
                 <div class="panel-body">
-                    This is your profile.
+                    <p>Name: {{ Auth::user()->name }}</p>
+                    <p>Email: {{ Auth::user()->email }}</p>
+                    <p>
+                        <a href="#">Change password.</a>
+                    </p>
+                    <p>
+                        <a href="#">Delete account.</a>
+                    </p>
                 </div>
             </div>
         </div>
