c72313f18944ed1159a711dcea46127d0b50bc73
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-12 16:27:41 +1000

    Added example panel for benefit of group.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,20 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            <!-- Panel 1 -->
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome!</div>
 
                 <div class="panel-body">
                     The quick brown fox jumps over the lazy dog.
+                </div>
+            </div>
+            <!-- Panel 2 -->
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome!</div>
+
+                <div class="panel-body">
+                    The quick brown dog jumps over the lazy fox.
                 </div>
             </div>
         </div>
