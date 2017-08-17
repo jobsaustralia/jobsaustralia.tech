93a3a8dd774c6ff2e8036947ce4cc8b5d902cbf5
rmit-s3491115-ozlem-kirmizi <s3491115@student.rmit.edu.au>
2017-08-17 15:39:47 +1000

    php changes
    php changes for user

--- app/User.php
+++ app/User.php
@@ -15,7 +15,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'job-title', 'location', 'sector', 'overall-exp', 'java', 'python', 'c', 'c-sharp', 'c-plus', 'php',
+        'html', 'css', 'javascript', 'sql', 'unix', 'win-server', 'win-desktop', 'linux-desktop', 'macos-desktop', 'pearl', 'bash', 'batch',
+        'cisco', 'microsoft-office', 'r', 'go', 'ruby', 'asp', 'scala',
     ];
 
     /**
