8a5708713f9df2093fcfb3f967b688466df102e7
kimluu1701 <kimmie@Kims-MacBook-Pro.local>
2017-08-17 16:05:02 +1000

    Homepage
    Updating job seekers’ homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,27 +4,40 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            
+            
             <!-- Panel 1 -->
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome!</div>
+                <div class="panel-heading"> <i class="fa fa-briefcase" aria-hidden="true" style="font-size: 30px"></i> Welcome!</div>
 
                 <div class="panel-body">
-                    The quick brown fox jumps over the lazy dog.
+                    <p> Jobsaustralia.tech is a job-matching website based in Melbourne, Australia focusing in the field of Information Technology, Computer Science and Software Engineering.</p>
                 </div>
             </div>
+
             <!-- Panel 2 -->
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome!</div>
+            	<div class="panel-heading"> <i class="fa fa-users" aria-hidden="true" style="font-size: 30px"></i> We're here for you</div>
 
-                <div class="panel-body">
-                    <p>
-                        <i class="fa fa-bath" aria-hidden="true" style="font-size: 30px"></i>
-                    </p>
-
-                    The quick brown dog jumps over the lazy fox.
+            	<div class="panel-body">
+                    <p> Match your skills and strengths with current job vacancies.</p>
+                    <p> Connect you with employers and jobs that complement your abilities.</p>
+                    <p> Market you to employers or work environment that is the most suitable for you.</p>
+                    <p> Help you to find a job in the field that you're passionate about.</p>
                 </div>
             </div>
+
+            <!-- Panel 3 -->
+            <div class="panel panel-default">
+            	<div class="panel-heading"><i class="fa fa-compass" aria-hidden="true" style="font-size: 30px"></i> The Guideline</div>
+            	<div class="panel-body">
+            	<p> <b>Step 1:</b> Register yourself with jobaustralia.tech. </p>
+            	<p> <b>Step 2:</b> Creat your own profile without having to submit your resume or cover letter. However, it is recommended that you pay high attention to listing your IT skills.</p>
+            	<p> <b>Step 3:</b> Wait for your notifications whenever we find you a great match with an employer ! </p>
+            	</div>
+            </div> 
         </div>
     </div>
 </div>
 @endsection
+ 
