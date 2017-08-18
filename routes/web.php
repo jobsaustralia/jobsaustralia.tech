02c3d0743b18d060730b4e0d26822351227d8fb2
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-18 11:54:24 +1000

    Testing returning model in JSON format for JS sorting.

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+// Remove this after changes.
+use App\User;
+
+Route::get('/', function (){
     return view('index');
 });
 
@@ -22,3 +25,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/profile', 'ProfileController@index')->name('profile');
 
 Route::post('/delete', 'Auth\DeleteController@delete')->name('delete');
+
+/* Example - Return all models in JSON format. */
+Route::get('/matches', function (){	
+	/* Obviously, we'll do this with Jobs. */
+	return User::all();
+});
