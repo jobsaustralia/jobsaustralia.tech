df6e9f60b3643fb2ba8a20b41bcdffaa7299f0a8
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-24 21:25:52 +1000

    Added all Job fields to match.js

--- public/js/match.js
+++ public/js/match.js
@@ -1,5 +1,5 @@
 /* Function to print job to panel. */
-function printJob(title, description){
+function printJob(title, description, hours, salary, availablefrom, location, startdate){
 	var display = document.getElementById("jobs");
 
 	var panel = document.createElement("div");
@@ -11,15 +11,52 @@ function printJob(title, description){
 
 	var body = document.createElement("div");
 	body.className = "panel-body";
-	body.innerHTML = description;
+
+	var p1 = document.createElement("p");
+	p1.innerHTML = description;
+
+	var hr1 = document.createElement("hr");
+
+	var p2 = document.createElement("p");
+	p2.innerHTML = hours;
+
+	var p3 = document.createElement("p");
+	p3.innerHTML = salary;
+
+	var p4 = document.createElement("p");
+	p4.innerHTML = availablefrom;
+
+	var p5 = document.createElement("p");
+	p5.innerHTML = location;
+
+	var p6 = document.createElement("p");
+	p6.innerHTML = startdate;
+
+	var hr2 = document.createElement("hr");
+
+	var p7 = document.createElement("p");
+
+	var apply = document.createElement("button");
+	apply.className = "btn btn-primary";
+	apply.innerHTML = "Apply";
 
 	panel.appendChild(heading);
 	panel.appendChild(body);
+	body.append(p1);
+	body.append(hr1);
+	body.append(p2);
+	body.append(p3);
+	body.append(p4);
+	body.append(p5);
+	body.append(p6);
+	body.append(hr2);
+	body.append(p7);
+	p7.append(apply);
 	display.appendChild(panel);
 }
 
 $.getJSON( "api/jobs", function(data){
 	for(i = 0; i < data.length; i++){
-		printJob(data[i].title, data[i].description);
+		printJob(data[i].title, data[i].description, data[i].hours, data[i].salary, data[i].availablefrom, data[i].location, data[i].startdate);
 	}
 });
