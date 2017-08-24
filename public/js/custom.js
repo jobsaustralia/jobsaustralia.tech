bba2dbb9fe943da3cc8b177db44aaf485544db2a
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-24 17:15:48 +1000

    Toggle buttons for profile.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,26 @@
 function showDeleteConfirmation(){
-	document.getElementById("confirm-delete-prompt").style.display = "block";
+	if(document.getElementById("confirm-delete-prompt").style.display == "none"){
+		document.getElementById("confirm-delete-prompt").style.display = "block";
+	}
+	else{
+		document.getElementById("confirm-delete-prompt").style.display = "none";
+	}
+	
+}
+
+function showChangePasswordMessage(){
+	if(document.getElementById("change-password-message").style.display == "none"){
+		document.getElementById("change-password-message").style.display = "block";
+	}
+	else{
+		document.getElementById("change-password-message").style.display = "none";
+	}
 }
 
 if(document.getElementById("confirm-delete") !== null){
 	document.getElementById("confirm-delete").addEventListener("click", showDeleteConfirmation);
+}
+
+if(document.getElementById("change-password") !== null){
+	document.getElementById("change-password").addEventListener("click", showChangePasswordMessage);
 }
