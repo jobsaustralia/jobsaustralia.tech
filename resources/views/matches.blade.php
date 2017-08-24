1f930cfacafaacde904e75203506c6a2d04cdaee
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-24 17:54:31 +1000

    Remove default panel.

--- resources/views/matches.blade.php
+++ resources/views/matches.blade.php
@@ -4,12 +4,7 @@
 <div class="container">
     <div class="row">
         <div id="jobs" class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">	Job Title</div>
-                <div class="panel-body">
-                    Body
-                </div>
-            </div>
+            
         </div>
     </div>
 </div>
