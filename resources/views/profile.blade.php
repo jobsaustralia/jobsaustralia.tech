c52faac12639e0ce380e0ee7a10d4eff4a97561d
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-25 20:30:30 +1000

    New element ids for JS improvements

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div id="profile" class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ Auth::user()->name }}'s Profile</div>
 
@@ -17,14 +17,14 @@
                             Change password
                         </button>
 
-                        <p id="change-password-message" style="display: none;">To change your password, Logout and select "Forgot Your Password".</p>
+                        <p id="change-password-content" style="display: none;">To change your password, Logout and select "Forgot Your Password".</p>
                     </p>
                     <p>
                         <button id="confirm-delete"> 
                             Delete account.
                         </button>
 
-                        <p id="confirm-delete-prompt" style="display: none;">Confirm deletion: <a href="{{ route('delete') }}" onclick="event.preventDefault(); document.getElementById('delete-form').submit();">I really want to delete my account.</a></p>
+                        <p id="confirm-delete-content" style="display: none;">Confirm deletion: <a href="{{ route('delete') }}" onclick="event.preventDefault(); document.getElementById('delete-form').submit();">I really want to delete my account.</a></p>
 
                         <form id="delete-form" action="{{ route('delete') }}" method="POST" style="display: none;">
                             {{ csrf_field() }}
