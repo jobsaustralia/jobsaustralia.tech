397ba452fa7ab86bdd3d499fdd0fc89762e866c7
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-25 20:29:01 +1000

    Pointed to correct Controller

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,11 +7,11 @@
             <div class="panel panel-default">
                 <div class="panel-heading">	Contact Us</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal" method="POST" action="{{ route('enquire') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="title" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
