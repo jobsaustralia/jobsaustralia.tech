e18e3078ae312b5b22012ecc86893abe842e6ed4
d473 <30338371+d473@users.noreply.github.com>
2017-08-28 21:53:31 +1000

    Updated profile view
    Lists all skills as 1 or 0. Added job title section and edit profile link.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,8 +10,35 @@
                 <div class="panel-body">
                     <p>Name: {{ Auth::user()->name }}</p>
                     <p>Email: {{ Auth::user()->email }}</p>
-					<p>Location: {{ Auth::user()->location }}</p>
+					          <p>Location: {{ Auth::user()->location }}</p>
                     <p>Sector: {{ Auth::user()->sector }}</p>
+                    <p>Job Title: {{ Auth::user()->title }}</p>
+                    <p>Skills: 1 Means Selected, 0 means not selected </p>
+                    <p>Java: {{Auth::user()->java }}</p>
+                    <p>Python: {{Auth::user()->python }}</p>
+                    <p>C: {{Auth::user()->c }}</p>
+                    <p>C#: {{Auth::user()->csharp }}</p>
+                    <p>C++: {{Auth::user()->cplus }}</p>
+                    <p>PHP: {{Auth::user()->php }}</p>
+                    <p>HTML: {{Auth::user()->html }}</p>
+                    <p>CSS: {{Auth::user()->css }}</p>
+                    <p>JavaScript: {{Auth::user()->javascript }}</p>
+                    <p>SQL: {{Auth::user()->sql }}</p>
+                    <p>UNIX: {{Auth::user()->unix }}</p>
+                    <p>Windows Server: {{Auth::user()->winserver }}</p>
+                    <p>Windows Desktop: {{Auth::user()->windesktop }}</p>
+                    <p>Linux Desktop: {{Auth::user()->linuxdesktop }}</p>
+                    <p>Mac OS Desktop: {{Auth::user()->macosdesktop }}</p>
+                    <p>Pearl: {{Auth::user()->pearl }}</p>
+                    <p>Bash: {{Auth::user()->bash }}</p>
+                    <p>Batch: {{Auth::user()->batch }}</p>
+                    <p>Cisco: {{Auth::user()->cisco }}</p>
+                    <p>Office: {{Auth::user()->office }}</p>
+                    <p>R: {{Auth::user()->r }}</p>
+                    <p>Go: {{Auth::user()->go }}</p>
+                    <p>Ruby: {{Auth::user()->ruby }}</p>
+                    <p>ASP: {{Auth::user()->asp }}</p>
+                    <p>Scala: {{Auth::user()->scala }}</p>
                     <p>
                         <button id="change-password">
                             Change password
@@ -19,8 +46,9 @@
 
                         <p id="change-password-content" style="display: none;">To change your password, Logout and select "Forgot Your Password".</p>
                     </p>
+                    <a href='edit'>Edit your profile</a>
                     <p>
-                        <button id="confirm-delete"> 
+                        <button id="confirm-delete">
                             Delete account.
                         </button>
 
