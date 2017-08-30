799094ea83af8c974cc204cb2cdcff0ab806e2fd
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-30 20:24:50 +1000

    OCD

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -16,28 +16,7 @@
                     <p>Experience: {{ Auth::user()->experience }} Year(s)</p>
 
                     <hr>
-					<p>
-					<form class="form-horizontal" method="POST" action="{{ route('upload') }}" enctype="multipart/form-data">
-                        {{ csrf_field() }}
 
-                        <div>
-                            <p>Resume (*optional)</p>
-
-                            <div class="col-md-6">
-                                <input type="file" class="form-control" name="file">
-
-                          <p>
-
-                        <div>
-								
-                                <button type="submit" class="btn btn-primary">
-                                    Upload resume.
-                                </button>
-								
-                            </div>
-                    </form>
-					</p>
-					<hr>
                     <p>
                         <button type="button" class="btn btn-primary" onclick="event.preventDefault(); document.getElementById('edit-form').submit();">
                             Edit profile and skills.
@@ -69,6 +48,38 @@
 
                     <form id="edit-form" action="{{ route('edit') }}" method="GET" style="display: none;">
                         {{ csrf_field() }}
+                    </form>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Upload Resume</div>
+
+                <div class="panel-body">
+                    <p>Uploading a resume is optional.</p>
+
+                    <form class="form-horizontal" method="POST" action="{{ route('upload') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('resume') ? ' has-error' : '' }}">
+                            <div class="col-md-6">
+                                <input id="resume" type="file" class="form-control" name="resume" required autofocus>
+
+                                @if ($errors->has('resume'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('resume') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <button type="submit" class="btn btn-primary">
+                                    Upload
+                                </button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
