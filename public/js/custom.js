9b80e99499d0cb12aee897047af26ffd4ce2054f
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-31 00:32:53 +1000

    "I'm a student" checkbox to autofill relevant fields.
    Student status is not stored in database.

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,9 +9,29 @@ function toggleDisplay(){
 	}
 }
 
+function studentFill(){
+	if(document.getElementById("student").checked){
+		document.getElementById("title").value = "Student";
+		document.getElementById("title").readOnly = true;
+		document.getElementById("sector").value = "None";
+		document.getElementById("sector").readOnly = true;
+		document.getElementById("experience").value = 0;
+	}
+	else{
+		document.getElementById("title").value = null;
+		document.getElementById("title").readOnly = false;
+		document.getElementById("sector").value = null;
+		document.getElementById("sector").readOnly = false;
+		document.getElementById("experience").value = null;
+	}
+}
+
 if(document.getElementById("profile") !== null){
 	document.getElementById("confirm-delete").addEventListener("click", toggleDisplay);
 	document.getElementById("change-password").addEventListener("click", toggleDisplay);
+}
+else if(document.getElementById("register") !== null){
+	document.getElementById("student").addEventListener("change", studentFill);
 }
 else if(document.getElementById("team") !== null){
 	document.getElementById("aaron").addEventListener("click", toggleDisplay);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div id="register" class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-body">
                     You're using JobsAustralia.tech as a <em>Job Seeker</em>. <a href="https://employ.jobsaustralia.tech/register">Change to Employer</a>.
@@ -43,6 +43,15 @@
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
+                            </div>
+                        </div>
+
+                        <!-- Student Check -->
+                        <div class="form-group">
+                            <label for="student" class="col-md-4 control-label">I'm a student</label>
+
+                            <div class="col-md-1">
+                                <input id="student" type="checkbox" class="form-control" value="1" autofocus>
                             </div>
                         </div>
 						
