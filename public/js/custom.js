a2f4169bb36299d086e8f1fe77a12606635aa574
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-31 21:13:41 +1000

    Randomise order of team list on about page, add image.
    Image needs to be 196px * 196px, a png, and follow my naming convention.

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,6 +9,52 @@ function toggleDisplay(){
     }
 }
 
+function toggleTeamDisplay(){
+    document.getElementById("aaron-content").style.display = "none";
+    document.getElementById("ozlem-content").style.display = "none";
+    document.getElementById("kim-content").style.display = "none";
+    document.getElementById("melissa-content").style.display = "none";
+    document.getElementById("dennis-content").style.display = "none";
+    document.getElementById(event.target.id + "-content").style.display = "block";
+}
+
+function randomiseTeam(){
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
+    }
+
+    var team = ["aaron,Aaron Horler", "ozlem,Ozlem Kirmizi", "kim,Kim Luu", "melissa,Melissa Nguyen", "dennis,Dennis Mihalache"];
+    shuffle(team);
+
+    var namesDiv = document.getElementById("names");
+
+    var i;
+    for(i = 0; i < team.length; i++){
+        var button = document.createElement("button");
+        button.id = team[i].split(",")[0];
+        button.className = "btn btn-primary";
+        button.style.marginTop = "5px";
+        button.style.marginBottom = "5px";
+        button.innerHTML = team[i].split(",")[1];
+        namesDiv.appendChild(button);
+        namesDiv.innerHTML += " ";
+    }
+
+    document.getElementById(team[0].split(",")[0] + "-content").style.display = "block";
+
+    document.getElementById("aaron").addEventListener("click", toggleTeamDisplay);
+    document.getElementById("ozlem").addEventListener("click", toggleTeamDisplay);
+    document.getElementById("kim").addEventListener("click", toggleTeamDisplay);
+    document.getElementById("melissa").addEventListener("click", toggleTeamDisplay);
+    document.getElementById("dennis").addEventListener("click", toggleTeamDisplay);
+}
+
 function studentFill(){
     if(document.getElementById("student").checked){
         document.getElementById("title").value = "Student";
@@ -34,9 +80,5 @@ else if(document.getElementById("register") !== null){
     document.getElementById("student").addEventListener("change", studentFill);
 }
 else if(document.getElementById("team") !== null){
-    document.getElementById("aaron").addEventListener("click", toggleDisplay);
-    document.getElementById("ozlem").addEventListener("click", toggleDisplay);
-    document.getElementById("melissa").addEventListener("click", toggleDisplay);
-    document.getElementById("kim").addEventListener("click", toggleDisplay);
-    document.getElementById("dennis").addEventListener("click", toggleDisplay);
+    document.addEventListener('DOMContentLoaded', randomiseTeam);
 }
