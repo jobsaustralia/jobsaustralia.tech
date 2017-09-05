71a2a60e14d7847b8e6353b0943f9e88d5647955
kimluu1701 <kim.possiblee1701@gmail.com>
2017-09-05 16:49:19 +1000

    Jobs Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
 		$this->call(EmployersTableSeeder::class);
+        $this->call(JobsTableSeeder::class);
     }
 }
