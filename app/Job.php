af5b462b837f91709db23150f9173a4c9806d78f
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-05 16:57:27 +1000

    Fixed Job model

--- app/Job.php
+++ app/Job.php
@@ -12,11 +12,8 @@ class Job extends Model
      *
      * @var array
      */
-<<<<<<< HEAD
-    protected $fillable = ['title', 'description', 'hours', 'salary', 'availablefrom', 'location', 'state', 'startdate', 'java', 'python', 'c', 'csharp', 'cplus', 'php', 'html', 'css', 'javascript', 'sql', 'unix', 'winserver', 'windesktop', 'linuxdesktop', 'macosdesktop', 'pearl', 'bash', 'batch', 'cisco', 'office', 'r', 'go', 'ruby', 'asp', 'scala'];
-=======
-    protected $fillable = ['title', 'description', 'hours', 'salary', 'startdate', 'state', 'city', 'java', 'python', 'c', 'csharp', 'cplus', 'php', 'html', 'css', 'javascript', 'sql', 'unix', 'winserver', 'windesktop', 'linuxdesktop', 'macosdesktop', 'pearl', 'bash', 'batch', 'cisco', 'office', 'r', 'go', 'ruby', 'asp', 'scala'];
->>>>>>> origin/master
+
+    protected $fillable = ['title', 'description', 'hours', 'salary', 'availablefrom', 'state', 'city', 'java', 'python', 'c', 'csharp', 'cplus', 'php', 'html', 'css', 'javascript', 'sql', 'unix', 'winserver', 'windesktop', 'linuxdesktop', 'macosdesktop', 'pearl', 'bash', 'batch', 'cisco', 'office', 'r', 'go', 'ruby', 'asp', 'scala', 'employerid'];
 
     protected $table = 'jobs';
 }
