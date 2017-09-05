189baf271422720a3bddbd173d709d26656f340c
rmit-s3491115-ozlem-kirmizi <s3491115@student.rmit.edu.au>
2017-09-05 16:13:16 +1000

    Create EmployersTableSeeder, Update to other files and fix matches.blade.php

--- resources/views/matches.blade.php
+++ resources/views/matches.blade.php
@@ -6,6 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Filter Matches</div>
+				<div class="panel-heading">Filter</div>
                 <div class="panel-body">
                     <div class="form-group">
                         <label for="state" class="col-md-4 control-label">State/Territory</label>
