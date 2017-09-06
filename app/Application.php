b0bca3809363a1148a1174d13fa9e4c56d1447cb
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-06 19:31:41 +1000

    Add jobid to model

--- app/Application.php
+++ app/Application.php
@@ -12,7 +12,7 @@ class Job extends Model
      *
      * @var array
      */
-    protected $fillable = ['userid', 'message', 'employerid'];
+    protected $fillable = ['userid', 'employerid', 'jobid', 'message'];
 
     protected $table = 'applications';
 }
