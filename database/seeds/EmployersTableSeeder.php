e0163b25918915f33106bd8441e5ef4382a6ed0b
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-12 21:25:13 +1000

    Add location to seeds

--- database/seeds/EmployersTableSeeder.php
+++ database/seeds/EmployersTableSeeder.php
@@ -16,6 +16,8 @@ class EmployersTableSeeder extends Seeder
             'id' => 'db930f90-17a8-468a-b094-7416f0c63e99',
             'name' => 'KPMG',
             'email' => 'kpmg@gmail.com',
+            'state' => 'vic',
+            'city' => 'Melbourne',
             'password' => bcrypt('secret')
         ]);
         
@@ -23,6 +25,8 @@ class EmployersTableSeeder extends Seeder
             'id' => '7c8b051d-62d7-4250-b679-4ffffc2d1d5f',
             'name' => 'Telstra',
             'email' => 'telstra@live.com',
+            'state' => 'vic',
+            'city' => 'Melbourne',
             'password' => bcrypt('secret')
         ]);
         
@@ -30,6 +34,8 @@ class EmployersTableSeeder extends Seeder
             'id' => '400fa8b2-f8f9-4c64-9b70-ea6ea2e1fe2d',
             'name' => 'Accenture',
             'email' => 'accenture@gmail.com',
+            'state' => 'vic',
+            'city' => 'Melbourne',
             'password' => bcrypt('secret')
         ]);
         
@@ -37,6 +43,8 @@ class EmployersTableSeeder extends Seeder
             'id' => '5ed27969-c2cc-4d02-b7f6-c1bf0b19d613',
             'name' => 'IBM',
             'email' => 'ibm@gmail.com',
+            'state' => 'vic',
+            'city' => 'Melbourne',
             'password' => bcrypt('secret')
         ]);
         
@@ -44,6 +52,8 @@ class EmployersTableSeeder extends Seeder
             'id' => '593e4d9f-e71e-468f-b38a-572afd5ba2f7',
             'name' => 'Medibank',
             'email' => 'medibank@gmail.com',
+            'state' => 'vic',
+            'city' => 'Melbourne',
             'password' => bcrypt('secret')
         ]);
     }
