5de93df7f99f1d5f8561d22a986ff6d2879f6b83
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-12 20:40:10 +1000

    Add Employer model to job seekers.
    So that Employer information can be accessed as an Object (OO).

---
+++ app/Employer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employer extends Model{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'name', 'email', 'state', 'city'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $table = 'employers';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = false;
+}
