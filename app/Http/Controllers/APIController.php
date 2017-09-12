f68d9d5b01d0031e1884f1a6c0711ebae6ff8cc2
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-12 21:26:37 +1000

    New API to return all jobs by an Employer.
    For employer profile.

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -22,8 +22,34 @@ class APIController extends Controller{
     }
 
     /* Get all Jobs by state to which the job seeker has not applied. */
-    public function getJobs($state){
+    public function getJobsByState($state){
         $jobs = Job::where('state', $state)->get();
+        $applications = Application::where('userid', Auth::user()->id)->get();
+
+        /* Populate an array of job IDs to which the job seeker has applied. */
+        $appliedJobIDs = array();
+        foreach($jobs as $job){
+            foreach($applications as $application){
+                if($application->jobid == $job->id){
+                    array_push($appliedJobIDs, $job->id);
+                }
+            }
+        }
+
+        /* Populate an array of jobs whose IDs are not in the above array. */
+        $unappliedJobs = array();
+        foreach($jobs as $job){
+            if(!in_array($job->id, $appliedJobIDs)){
+                array_push($unappliedJobs, Job::findOrFail($job->id));
+            }
+        }
+
+        return $unappliedJobs;
+    }
+
+    /* Get all Jobs by state to which the job seeker has not applied. */
+    public function getJobsByEmployer($employerid){
+        $jobs = Job::where('employerid', $employerid)->get();
         $applications = Application::where('userid', Auth::user()->id)->get();
 
         /* Populate an array of job IDs to which the job seeker has applied. */
