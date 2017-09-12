479fa04b2736a27def3da69fdc235f182f6f1cf9
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-12 17:46:33 +1000

    Apply UUIDs to seeds

--- database/seeds/EmployersTableSeeder.php
+++ database/seeds/EmployersTableSeeder.php
@@ -13,30 +13,35 @@ class EmployersTableSeeder extends Seeder
     public function run()
     {
         DB::table('employers')->insert([
+            'id' => 'db930f90-17a8-468a-b094-7416f0c63e99',
             'name' => 'KPMG',
             'email' => 'kpmg@gmail.com',
             'password' => bcrypt('secret')
         ]);
         
         DB::table('employers')->insert([
+            'id' => '7c8b051d-62d7-4250-b679-4ffffc2d1d5f',
             'name' => 'Telstra',
             'email' => 'telstra@live.com',
             'password' => bcrypt('secret')
         ]);
         
         DB::table('employers')->insert([
+            'id' => '400fa8b2-f8f9-4c64-9b70-ea6ea2e1fe2d',
             'name' => 'Accenture',
             'email' => 'accenture@gmail.com',
             'password' => bcrypt('secret')
         ]);
         
         DB::table('employers')->insert([
+            'id' => '5ed27969-c2cc-4d02-b7f6-c1bf0b19d613',
             'name' => 'IBM',
             'email' => 'ibm@gmail.com',
             'password' => bcrypt('secret')
         ]);
         
         DB::table('employers')->insert([
+            'id' => '593e4d9f-e71e-468f-b38a-572afd5ba2f7',
             'name' => 'Medibank',
             'email' => 'medibank@gmail.com',
             'password' => bcrypt('secret')
