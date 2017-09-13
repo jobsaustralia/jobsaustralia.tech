62a67aed62847b28f3180003c32a69db2db770ed
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-13 16:43:42 +1000

    Cosmetic changes to homepage (index)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,34 +4,45 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <!-- Panel 1 -->
             <div class="panel panel-default">
-                <div class="panel-heading"> <i class="fa fa-briefcase" aria-hidden="true" style="font-size: 30px"></i> <b>WELCOME!</b></div>
+                <div class="panel-heading"><strong>Welcome</strong></div>
 
-                <div class="panel-body">
-                    <p> JobsAustralia.tech is a job-matching website based in Melbourne, Australia focusing in the field of Information Technology, Computer Science and Software Engineering.</p>
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-briefcase" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p>JobsAustralia.tech is a job matchmaking website based in Melbourne, Australia focusing in the fields of <strong>Information Technology</strong>, <strong>Computer Science</strong>, and <strong>Software Engineering</strong>.</p><br>
+                    <p><a class="btn btn-primary" @if (Auth::guest()) href="{{ route('register') }}">Register now @else href="{{ route('matches') }}">View matches @endif</a></p>
                 </div>
             </div>
 
-            <!-- Panel 2 -->
             <div class="panel panel-default">
-                <div class="panel-heading"> <i class="fa fa-users" aria-hidden="true" style="font-size: 30px"></i><b> WE'RE HERE FOR YOU </b></div>
+                <div class="panel-heading"><strong>Algorithmic matchmaking</strong></div>
 
-                <div class="panel-body">
-                    <p> Match your skills and strengths with current job vacancies.</p>
-                    <p> Connect you with employers and jobs that complement your abilities.</p>
-                    <p> Market you to employers or work environment that is the most suitable for you.</p>
-                    <p> Help you to find a job in the field that you're passionate about.</p>
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-calculator" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p>We use an advanced matchmaking algorithm to match jobs to your skillset.</p>
+                    <p>After you register, matching jobs are listed in order by a <strong>0% to 100%</strong> match depending on the requirements of the job and your self-reported skillset.</p>
                 </div>
             </div>
 
-            <!-- Panel 3 -->
             <div class="panel panel-default">
-                <div class="panel-heading"><i class="fa fa-compass" aria-hidden="true" style="font-size: 30px"></i><b> THE GUIDELINE </b></div>
-                <div class="panel-body">
-                <p> <b>Step 1:</b> Register yourself with jobaustralia.tech. </p>
-                <p> <b>Step 2:</b> Creat your own profile without having to submit your resume or cover letter. However, it is recommended that you pay high attention to listing your IT skills.</p>
-                <p> <b>Step 3:</b> Wait for your notifications whenever we find you a great match with an employer! </p>
+                <div class="panel-heading"><strong>We're here for you</strong></div>
+
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-users" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p>&bull; Match your skills and strengths with current job vacancies.</p>
+                    <p>&bull; Connect you with employers and jobs that complement your abilities.</p>
+                    <p>&bull; Market you to employers or work environment that is the most suitable for you.</p>
+                    <p>&bull; Help you to find a job in the field that you're passionate about.</p>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong>The guideline</strong></div>
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-compass" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p><strong>Step 1:</strong> Register yourself with JobsAustralia.tech</p>
+                    <p><strong>Step 2:</strong> Creat your own profile without having to submit your resume or cover letter. However, it is recommended that you pay high attention to listing your IT skills.</p>
+                    <p><strong>Step 3:</strong> Wait for your notifications whenever we find you a great match with an employer!</p>
                 </div>
             </div>
         </div>
