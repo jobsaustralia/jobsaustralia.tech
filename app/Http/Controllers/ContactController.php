ad69089e070a3e75507219772783f19a3b5a1e66
Aaron Horler <aghorler@users.noreply.github.com>
2017-10-03 17:32:48 +1100

    Group contact form.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,9 +11,29 @@ class ContactController extends Controller{
 
     /* Send an email. */
     function send(Request $request){
-        Mail::raw($request['message'] function($message){
-        $message->subject('This is a test.');
-        $message->to('s3481341@student.rmit.edu.au', 's3491115@student.rmit.edu.au');
-		});
+        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+            $message->subject('Contact form submission');
+            $message->to('s3481341@student.rmit.edu.au');
+        });
+
+        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+            $message->subject('Contact form submission');
+            $message->to('s3434719@student.rmit.edu.au');
+        });
+
+        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+            $message->subject('Contact form submission');
+            $message->to('s3491115@student.rmit.edu.au');
+        });
+
+        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+            $message->subject('Contact form submission');
+            $message->to('s3476694@student.rmit.edu.au');
+        });
+
+        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+            $message->subject('Contact form submission');
+            $message->to('s3536578@student.rmit.edu.au');
+        });
     }
 }
