5b8392d56e0180ad3c3f1d0a14f4872f2483da20
Aaron Horler <aghorler@users.noreply.github.com>
2017-10-04 15:30:23 +1100

    Backend for group contact form.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,34 +6,44 @@ use Mail;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class ContactController extends Controller{
 
-    /* Send an email. */
+    /* Send an email to each team member. */
     function send(Request $request){
-        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+        $email = $request['email'];
+
+        Mail::raw($request['message'] . "\n\n" . 'From: ' . $request['name'] . "\n\n" . 'IP: ' . request()->ip(), function($message) use($email){
             $message->subject('Contact form submission');
             $message->to('s3481341@student.rmit.edu.au');
+            $message->replyTo($email);
         });
 
-        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+        Mail::raw($request['message'] . "\n\n" . 'From: ' . $request['name'] . "\n\n" . 'IP: ' . request()->ip(), function($message) use($email){
             $message->subject('Contact form submission');
             $message->to('s3434719@student.rmit.edu.au');
+            $message->replyTo($email);
         });
 
-        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+        Mail::raw($request['message'] . "\n\n" . 'From: ' . $request['name'] . "\n\n" . 'IP: ' . request()->ip(), function($message) use($email){
             $message->subject('Contact form submission');
             $message->to('s3491115@student.rmit.edu.au');
+            $message->replyTo($email);
         });
 
-        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+        Mail::raw($request['message'] . "\n\n" . 'From: ' . $request['name'] . "\n\n" . 'IP: ' . request()->ip(), function($message) use($email){
             $message->subject('Contact form submission');
             $message->to('s3476694@student.rmit.edu.au');
+            $message->replyTo($email);
         });
 
-        Mail::raw($request['message'] . '. From: ' . $request['email'], function($message){
+        Mail::raw($request['message'] . "\n\n" . 'From: ' . $request['name'] . "\n\n" . 'IP: ' . request()->ip(), function($message) use($email){
             $message->subject('Contact form submission');
             $message->to('s3536578@student.rmit.edu.au');
+            $message->replyTo($email);
         });
+
+        return Redirect::route('contact');
     }
 }
